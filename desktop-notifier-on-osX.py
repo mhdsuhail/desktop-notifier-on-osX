941ea3863be378a5a29953d928aeced280c421ec
mhdsuhail <51886968+mhdsuhail@users.noreply.github.com>
2020-03-18 14:25:31 +0530

    Add files via upload

---
+++ desktop-notifier-on-osX.py
@@ -0,0 +1,8 @@
+import os
+
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+
+notify("Helo", "Desktop notification...")
